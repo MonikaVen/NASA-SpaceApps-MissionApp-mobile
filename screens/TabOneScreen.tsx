03680efdd1c4d8093003179f160b3ef80a4c4ed2
MonikaVen <monika.venckauskaite@gmail.com>
2020-09-30 00:00:36 +0300

    Added hello to orbiter

---
+++ screens/TabOneScreen.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { Button, StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import axios from 'axios';
+
+
+export default function TabOneScreen() {
+  const [messageState, setMessageState] = React.useState('None')
+  function retrieveMessage() {
+    // let message = fetch('http://ec2-52-15-90-21.us-east-2.compute.amazonaws.com:3000/');
+    setMessageState('Retrieving message...');
+    axios.get('http://ec2-52-15-90-21.us-east-2.compute.amazonaws.com:3000/').then((res)=> setMessageState(res.data))
+  }
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Mission</Text>
+      <Text>{messageState}</Text>
+      <Button title="Say hello to Mars Orbiter!" onPress={retrieveMessage}></Button>
+      <Text></Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.js" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
