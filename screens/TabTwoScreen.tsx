fce746b5f4cf9b26a1ec3edb1611ebac8b3895f5
MonikaVen <monika.venckauskaite@gmail.com>
2020-10-03 01:21:18 +0300

    Messaging

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button, StyleSheet, TextInput } from 'react-native';
 import * as Battery from 'expo-battery';
 import * as Location from 'expo-location';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+// import {
+//   Accelerometer,
+//   Barometer,
+//   Gyroscope,
+//   Magnetometer,
+//   MagnetometerUncalibrated,
+//   Pedometer,
+// } from 'expo-sensors';
 import axios from 'axios';
 export default function TabTwoScreen() {
   const [momentState, setMomentState] = React.useState({
+    id: '',
     x: null,
     y: null,
     z: null,
     t: null,
     speed: null,
-  })
-  const [batteryState, setBatteryState] = React.useState({
     b: null,
   })
   const [dataSentMessage, setDataSentMessage ] = React.useState(
@@ -25,32 +32,38 @@ export default function TabTwoScreen() {
         setErrorMsg('Permission to access location was denied');
       }
       let locationStamp = await Location.getCurrentPositionAsync({});
-      setMomentState({x: locationStamp.coords.latitude,
-      y: locationStamp.coords.longitude,
-      z: locationStamp.coords.altitude,
-      t: locationStamp.timestamp,
-      speed: locationStamp.coords.speed
+      let battery = await batteryLevel();
+      setMomentState({
+        id: 777,
+        x: locationStamp.coords.latitude,
+        y: locationStamp.coords.longitude,
+        z: locationStamp.coords.altitude,
+        t: locationStamp.timestamp,
+        speed: locationStamp.coords.speed,
+        b: battery
       })
       console.log(locationStamp.coords)
   }
   const batteryLevel = async () => {
     const batteryLevel = await Battery.getBatteryLevelAsync();
-    setBatteryState({ b: batteryLevel });
-    Battery.addBatteryLevelListener(({ batteryLevel }) => {
-    setBatteryState({ b: batteryLevel });
-    console.log('batteryLevel changed!', batteryLevel);
-  });
+    return batteryLevel
   }
+  const [value, onChangeText] = React.useState('');
+  const [message, setMessage] = React.useState({ message: value});
   const update = async () => {
-    batteryLevel();
     getLocationStamp();
   }
   const sendData = () => {
     console.log('Sending ...')
-    axios.post('http://ec2-52-15-90-21.us-east-2.compute.amazonaws.com:3000/rover', batteryState.b).then((res)=> setDataSentMessage(JSON.stringify(res.data)))
     axios.post('http://ec2-52-15-90-21.us-east-2.compute.amazonaws.com:3000/rover', momentState).then((res)=> setDataSentMessage(JSON.stringify(res.data)))
+    //axios.post('http://ec2-52-15-90-21.us-east-2.compute.amazonaws.com:3000/rover', momentState).then((res)=> setDataSentMessage(JSON.stringify(res.data)))
 
   }
+  const sendMessage = () => {
+    setMessage({message: value})
+    axios.post('http://ec2-52-15-90-21.us-east-2.compute.amazonaws.com:3000/earth', message).then((res) => console.log(res));
+  } 
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Rover Data</Text>
@@ -58,11 +71,18 @@ export default function TabTwoScreen() {
       <Text>y: {momentState.y}</Text>
       <Text>z: {momentState.z}</Text>
       <Text>t: {momentState.t}</Text>
-      <Text>b: {batteryState.b*100}%</Text>
+      <Text>b: {momentState.b*100}%</Text>
       <Text>speed: {momentState.speed}</Text>
       <Button title="Update" onPress={update}></Button>
-      <Text>{dataSentMessage}</Text>
+      {/* <Text>{dataSentMessage}</Text> */}
       <Button title="Send data" onPress={sendData}></Button>
+      <TextInput 
+        placeholder="Type message for Earth"
+        onChangeText={text => onChangeText(text)}
+        value={value}
+        blurOnSubmit={true}
+      />
+      <Button onPress={sendMessage}></Button>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.js" />
     </View>
